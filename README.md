901ca2a3e4670ac5feb1e4d5a0466568da96ce54
Mark Marzeotti <mark@markmarzeotti.com>
2020-05-20 08:17:06 -0400

    built v1 of plugin

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# multi-category-permalinks
-Allow multiple categories in the permalink structure
+# Multi Category Permalinks
+
+Allow a varying number of categories in the post permalink structure.
+
+This plugin solves a problem I come across often when working with SEO teams. It will allow multiple categories in the permalink structure instead of being locked into just one plus the post name. I'm not a fan of the custom permalink type plugins as it requires a lot of discipline to properly silo your content. When working on a site with a team that may include designers, SEOs, content writers, editors, marketers, or other devs, it can be hard to keep track of the content strategy. If anyone posting a new post also has to determine the url structure, things can slip through the cracks.
+
+After building this plugin my posts had the following URLs:
+
+```
+https://example.com/snippets/this-is-a-post/
+https://example.com/snippets/javascript/this-is-another-post/
+https://example.com/snippets/javascript/es6/this-is-a-third-post/
+```
+
+This did not get in the way of pages and building URLs with a parent-child relationship or any other URLs built by WordPress (like feeds or archives).
+
+This plugin does edit the post permalink using the [post_link](https://developer.wordpress.org/reference/hooks/post_link/) filter, so it does not take the permalink settings in `Settings > Permalinks` in the WordPress admin into account. (I would like to eventually add that functionality however.) This plugin should function as described regardless of what is set on the Permalink Settings page.
---
+++ multi-category-permalinks.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Multi Category Permalinks
+ *
+ * @link              https://markmarzeotti.com/
+ * @since             1.0.0
+ * @package           Multi_Category_Permalinks
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Multi Category Permalinks
+ * Plugin URI:        https://markmarzeotti.com/
+ * Description:       Allow multiple categories in the permalink structure.
+ * Version:           1.0.0
+ * Author:            Mark Marzeotti
+ * Author URI:        https://markmarzeotti.com/
+ * Text Domain:       multi-category-permalinks
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
+
+/**
+ * Flush the rewrite rules after activation or deactivation.
+ */
+function mcp_flush_rewrite_rules() {
+	flush_rewrite_rules();
+}
+
+register_activation_hook( __FILE__, 'mcp_flush_rewrite_rules' );
+register_deactivation_hook( __FILE__, 'mcp_flush_rewrite_rules' );
+
+/**
+ * Use URL Structure taxonomy in post link.
+ *
+ * Use `/%postname%/` when selecting a custom permalink on the permalink settings page.
+ * This function is not looking for anything to replace.
+ *
+ * @param string         $permalink The post's permalink.
+ * @param WP_Post_Object $post      The post in question.
+ * @param boolean        $leavename Whether to keep the post name.
+ */
+function mcp_modify_post_link( $permalink, $post, $leavename ) {
+    $home_url     = get_home_url();
+    // Get the list of terms (categories) attached to this post.
+    $categories   = get_the_terms( $post->ID, 'category' );
+    // Pluck each term's ID into its own array.
+    $category_ids = array_column( $categories, 'term_id' );
+    // Get terms in order of heirarchy, but limited to the terms attached to this post.
+    $categories   = get_terms( 
+        array( 
+            'taxonomy' => 'category', 
+            'orderby' => 'parent', 
+            'include' => $category_ids 
+        ) 
+    );
+    $url_base     = '';
+    $last_term    = null;
+    if ( ! empty( $categories ) ) {
+        $url_base = '/';
+        foreach ( $categories as $key => $term ) {
+            // This has to be the first term in the list or a child of the previous term to be appended.
+            if ( null === $last_term || $term->parent === $last_term ) {
+                $url_base .= 0 === $key ? $term->slug : '/' . $term->slug;
+                $last_term = $term->term_id;
+            }
+        }
+    }
+    // $permalink = str_replace( $home_url, $home_url . $url_base, $permalink );
+    $permalink = $home_url . $url_base . '/' . $post->post_name . '/';
+    return $permalink;
+}
+add_filter( 'post_link', 'mcp_modify_post_link', 10, 3 );
+
+/**
+ * Add a rewrite rule allowing multiple prefixes in URL structure.
+ */
+function mcp_add_rewrite_rule() {
+    add_rewrite_rule( '(.+?)/[a-z-]*?([^/]+)/?$', 'index.php?taxonomy=category&term=$matches[1]&name=$matches[2]', 'bottom' );
+}
+add_action( 'init', 'mcp_add_rewrite_rule' );
